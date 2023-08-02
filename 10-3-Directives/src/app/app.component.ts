7861e483496cafba3c9a0cd610c8621d4bb2c53e
DH0516 <kood7567@gmail.com>
2023-08-01 23:57:05 -0400

    10-3 mofdified

--- 10-3-Directives/src/app/app.component.ts
+++ 10-3-Directives/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title= '10-3-Directives';
   photo: String = "https://clipart-library.com/img1/1220170.jpg";
 }
